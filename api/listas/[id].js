dafcca69197ab1026baff777bdb9ff00c0f6f0e0
delci-deeprocket <delci@deeprocket.com.br>
2025-04-28 21:12:24 -0300

    refactor: update CORS headers to include Authorization support

--- api/listas/[id].js
+++ api/listas/[id].js
@@ -6,7 +6,7 @@ const dataPath = path.resolve('./data.json');
 export default function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*'); // Allow all origins
   res.setHeader('Access-Control-Allow-Methods', 'GET, PUT, DELETE, POST, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
   if (req.method === 'OPTIONS') {
     return res.status(200).end(); // Handle preflight requests
